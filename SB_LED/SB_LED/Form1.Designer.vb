c53799ee88e52c6190c6e0abae4322a6258bccb2
jaspers0324 <32628620+jaspers0324@users.noreply.github.com>
2020-01-26 11:13:41 +0800

    Add files via upload

---
+++ SB_LED/SB_LED/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(39, 84)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Auto Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(149, 84)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 30)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Auto Close"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(149, 132)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(90, 30)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Plate Insert"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(149, 268)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(90, 30)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Door Close"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(149, 177)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(90, 30)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Probe Contact"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(149, 222)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(90, 30)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Ping DUT"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(39, 132)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(90, 30)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Plate Back"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(39, 268)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(90, 30)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Door Open"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(39, 177)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(90, 30)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Probe Up"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(39, 222)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(90, 30)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "DUT Dis-Link"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(260, 84)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(219, 214)
+        Me.RichTextBox1.TabIndex = 10
+        Me.RichTextBox1.Text = ""
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(118, 27)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 12)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "COM  Port :  "
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(260, 27)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 13
+        Me.Button11.Text = "Connect"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(323, 338)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 12)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Check Fixture Action Tool V1.0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(515, 359)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ SB_LED/SB_LED/Form1.vb
@@ -0,0 +1,210 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+
+Public Class Form1
+    Shared _continue As Boolean
+    Shared _serialPort As SerialPort
+
+    Dim comPORT As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        comPORT = ""
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(sp)
+        Next
+
+
+
+    End Sub
+
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If (ComboBox1.SelectedItem <> "") Then
+            comPORT = ComboBox1.SelectedItem
+        End If
+    End Sub
+
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+        If (Button11.Text = "Connect") Then
+            If (comPORT <> "") Then
+                SerialPort1.Close()
+                'SerialPort1.PortName = "com7" 'change com port to match your Arduino port
+                SerialPort1.PortName = comPORT
+                SerialPort1.BaudRate = 9600
+                SerialPort1.DataBits = 8
+                SerialPort1.Parity = Parity.None
+                SerialPort1.StopBits = StopBits.One
+                SerialPort1.Handshake = Handshake.None
+                SerialPort1.Encoding = System.Text.Encoding.Default
+
+                SerialPort1.Open()
+                Button11.Text = "Dis-connect"
+
+                '連接COM Port 先將單一關門功能鎖住
+                Button4.Enabled = False
+            End If
+
+        Else
+            SerialPort1.Close()
+            Button11.Text = "Connect"
+            RichTextBox1.Text = ""
+
+        End If
+
+    End Sub
+
+    'Auto Open 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("0")
+        'SerialPort1.Close()
+
+        '治具打開無動作時可以做單一動作檢查控制
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+        Button10.Enabled = True
+        RichTextBox1.Text &= "Box Auto Open..." & vbCrLf
+
+    End Sub
+
+    'Auto Close 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("1")
+        'SerialPort1.Close()
+
+        '治具關起來時關閉所有單一檢查控制功能
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+        Button10.Enabled = False
+        RichTextBox1.Text &= "Box Auto Close..." & vbCrLf
+
+    End Sub
+
+    'Plate Insert
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("3")
+        'SerialPort1.Close()
+
+        '載板進入治具關閉開關門功能
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button4.Enabled = False
+        Button8.Enabled = False
+        RichTextBox1.Text &= "Plate Insert..." & vbCrLf
+    End Sub
+
+    'Door Close
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("5")
+        'SerialPort1.Close()
+
+        'Door Close 關閉 DUT Dis-Link 避免誤觸動作並開啟 Door Open功能
+        Button8.Enabled = True
+        Button10.Enabled = False
+        RichTextBox1.Text &= "Door Close..." & vbCrLf
+
+    End Sub
+
+    'Probe Contact
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("7")
+        'SerialPort1.Close()
+
+        'Probe Contact 時關閉所有Auto Close/Open動作並關閉Plate Back功能避免誤觸動作
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button4.Enabled = False
+        Button8.Enabled = False
+        Button7.Enabled = False
+
+        RichTextBox1.Text &= "Probe Contact..." & vbCrLf
+    End Sub
+
+    'Ping DUT
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("9")
+        'SerialPort1.Close()
+
+        'Ping DUT 開啟 Door Open動作並關閉Close/Open動作並關閉Probe Up功能避免誤觸動作
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+
+        Button4.Enabled = True
+
+        RichTextBox1.Text &= "Ping DUT..." & vbCrLf
+    End Sub
+
+    'Plate Back
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("2")
+        'SerialPort1.Close()
+
+        'Plate Back 關閉 Door Close動作並開啟Close/Open功能
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button8.Enabled = True
+
+        Button4.Enabled = False
+        RichTextBox1.Text &= "Plate Back..." & vbCrLf
+    End Sub
+
+    'Door Open
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("4")
+        'SerialPort1.Close()
+
+        'Door Open 開啟 DUT Dis-Link  
+        Button10.Enabled = True
+        RichTextBox1.Text &= "Door Open..." & vbCrLf
+    End Sub
+
+    'Probe Up
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("6")
+        'SerialPort1.Close()
+
+        'Probe Up 開啟 Plate Back  
+        Button7.Enabled = True
+        RichTextBox1.Text &= "Probe Up..." & vbCrLf
+
+    End Sub
+
+    'DUT Dis-Link
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        'SerialPort1.Open()
+        SerialPort1.Write("8")
+        'SerialPort1.Close()
+
+        ' DUT-Dis Link 開啟 Probe Up 
+        Button9.Enabled = True
+        RichTextBox1.Text &= "DUT Dis-Link..." & vbCrLf
+
+    End Sub
+
+
+End Class
